951375890170e3cd6e6abea7c02d18ee15eb98ac
Sergey Tyapkin <Tyapkin2002@mail.ru>
2021-02-27 01:26:12 +0300

    Сделал кнопку регистрации на /about

--- public/pages/about.html.js
+++ public/pages/about.html.js
@@ -1,14 +1,14 @@
 export const html = `
 <div class="left-item" style="padding: 20px">
-    <div class="title-big">Куда ты попал?</div>
-    <div class="text"><span class="text-big">Здесь</span> ты сможешь найти <span class="text-big">квест</span> по душе, созданный нашими профессионалами.</div>
+    <div class="title-big">КУДА ТЫ ПОПАЛ?</div>
+    <div class="text"><span class="text-big">Здесь</span> ты сможешь найти <span class="text-big">онлайн-квест</span> по душе, созданный нашими професси<span style="font-style: italic">онал</span>ами.</div>
 </div>
 <div class="right-item" style="padding: 20px">
-    <div class="title">Хоть бесплатно?</div>
+    <div class="title">ЭТО ХОТЬ БЕСПЛАТНО?</div>
     <div class="text">Всё <span class="text-big">бесплатно</span> <span style="text-decoration: line-through">пока что</span>.</div>
 </div>
 <div class="left-item" style="padding: 20px">
-    <div class="title-big">Как пользоваться?</div>
+    <div class="title-big">КАК ПОЛЬЗОВАТЬСЯ?</div>
     <div class="text-big">Смотри, всё просто:
         <div style="padding-left: 10px">
             <li class="text">регистрируешься</li>
@@ -18,25 +18,27 @@ export const html = `
     </div>
 </div>
 <div class="right-item" style="padding: 20px">
-    <div class="title">У меня остались вопросы</div>
+    <div class="title">У МЕНЯ ОСТАЛИСЬ ВОПРОСЫ</div>
     <div class="text">Там снизу есть <span class="text-big">контакты</span> - пиши, не стестняйся.</div>
 </div>
-<linkButton class="submit center">Регистрация</linkButton>
+<div style="position: relative; text-align: center; margin: 30px">
+    <linkButton class="submit light" href="/register">Регистрация</linkButton>
+</div>
 
 <ul class="underbar-contacts">
     <!--div><li style="padding: 0px 0 0px 10px">
         <span>Контакты</span>
     </li></div-->
     <li>
-        <div class="text">E-mail:</div>
+        <div class="title" style="font-size: 18px; font-family: Arial">E-mail:</div>
         <div style="text-shadow: 0 0 8px #ba9b87">Tyapkin2002@mail.ru</div>
     </li>
     <li>
-        <div class="text">VK: <span style="font-size: 8px">(лучше сюда)</span> </div>
+        <div class="title" style="font-size: 18px; font-family: Arial">VK: <span style="font-size: 8px">(лучше сюда)</span> </div>
         <div><a href="https://vk.com/0pointer" style="color: inherit; text-shadow: 0 0 8px #baa190">vk.com/0pointer</a></div>
     </li>
     <li>
-        <div class="text">Telegram:</div>
+        <div class="title" style="font-size: 18px; font-family: Arial">Telegram:</div>
         <div><a href="https://t.me/tyapkin_s" style="color: inherit; text-shadow: 0 0 8px #c3a38e">t.me/tyapkin_s</a></div>
     </li>
 </ul>
